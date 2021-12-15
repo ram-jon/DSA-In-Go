b29c8bcb4e601bb94cac566d77e12087ff9994b6
Ram Jondhale <ram.jondhale@perennialsys.com>
2021-12-15 17:31:55 +0530

    Add error messages to stacks pop and top methods

--- Stack.go
+++ Stack.go
@@ -14,22 +14,26 @@ func (S *Stack) Size() int {
 	return len(S.stack)
 }
 
-func (S *Stack) Top() int {
-	return S.stack[len(S.stack)-1]
+func (S *Stack) Top() (int, error) {
+	if len(S.stack) > 0 {
+		return S.stack[len(S.stack)-1], nil
+	} else {
+		return -1, fmt.Errorf("Stack is empty")
+	}
 }
 
 func (S *Stack) Push(data int) {
 	S.stack = append(S.stack, data)
 }
 
-func (S *Stack) Pop() int {
+func (S *Stack) Pop() (int, error) {
 	size := len(S.stack)
 	if size > 0 {
 		data := S.stack[size-1]
 		S.stack = S.stack[:size-1]
-		return data
+		return data, nil
 	} else {
-		return -1
+		return -1, fmt.Errorf("Stack is empty can't pop")
 	}
 }
 
@@ -48,11 +52,20 @@ func main() {
 	fmt.Println("Is stack empty: ", S.Empty())
 
 	// print top of the stack
-	fmt.Println("Top of the stack: ", S.Top())
+	top, _ := S.Top()
+	fmt.Println("Top of the stack: ", top)
 
 	// remove element from stack
-	fmt.Println(S.Pop())
-	fmt.Println(S.Pop())
-	fmt.Println(S.Pop())
-
+	top, _ = S.Pop()
+	fmt.Println(top)
+	top, _ = S.Pop()
+	fmt.Println(top)
+	top, _ = S.Pop()
+	fmt.Println(top)
+	top, ok := S.Pop()
+	if ok != nil {
+		fmt.Println(ok)
+	} else {
+		fmt.Println(top)
+	}
 }
